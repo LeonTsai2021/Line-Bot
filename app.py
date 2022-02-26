ee212669cc948a7eafeeca346b538177973e870f
Leon Tsai <deathknight285@gmail.com>
2022-02-26 15:34:43 +0800

    Update

--- app.py
+++ app.py
@@ -38,7 +38,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg=event.message.text
-    s='你吃飯了嗎'
+    reply='我看不懂你說甚麼'
+    
+    if msg=='hi': 
+        r='Hi'
+    elif msg=='你吃飯了嗎?':
+        r='還沒'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
